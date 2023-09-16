d850b9ab501af6934fb735b484ef29d3b5a9345c
Sam Lin <maxisam@gmail.com>
2023-09-16 12:58:26 -0500

    ✨feature: add test case and workflow

---
+++ __tests__/dotnet/Console/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// create an example code to do a test
+int sum = 0;
+string s = "Hello";
+for (int i = 1; i <= 5; i++)
+{
+                 sum += i;   
+}
+Console.WriteLine(sum);
+
