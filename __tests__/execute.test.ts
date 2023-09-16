d4fdc71e1be24102f6f986edd384a71524d4f2f7
Sam Lin <maxisam@gmail.com>
2023-09-16 11:20:58 -0500

    add test

---
+++ __tests__/execute.test.ts
@@ -0,0 +1,18 @@
+import {describe, expect, it} from '@jest/globals';
+import {execute} from '../src/execute';
+
+describe('execute', () => {
+  it('should execute a command successfully', async () => {
+    // Arrange
+    const cmd = 'echo';
+    const args = ['hello', 'world'];
+
+    // Act
+    const result = await execute(cmd, process.cwd(), args, false, false);
+
+    // Assert
+    expect(result.result).toBe(true);
+    expect(result.stdout.join('')).toBe('hello world\n');
+    expect(result.stderr.join('')).toBe('');
+  });
+});
