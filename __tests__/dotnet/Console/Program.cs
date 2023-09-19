1f85b0f6a33143025d9ef6f5716058f6014e56ab
Sam Lin <maxisam@gmail.com>
2023-09-19 12:26:35 -0500

    test

--- __tests__/dotnet/Console/Program.cs
+++ __tests__/dotnet/Console/Program.cs
@@ -2,10 +2,25 @@
 Console.WriteLine("Hello, World!");
 // create an example code to do a test
 int sum = 0;
-string s = "Hello";
+string notUsed;
 for (int i = 1; i <= 5; i++)
 {
-    sum += i;
+      sum += i;
 }
 Console.WriteLine(sum);
 
+for (int i = 1; i <= 5; i++)
+{
+      sum += i;
+}
+Console.WriteLine(sum);
+for (int i = 1; i <= 5; i++)
+{
+      sum += i;
+}
+Console.WriteLine(sum);
+for (int i = 1; i <= 5; i++)
+{
+      sum += i;
+}
+Console.WriteLine(sum);
